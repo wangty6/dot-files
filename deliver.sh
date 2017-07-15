0253b8a0339f5d75980e65acfdec1bf5c268afc2
tianyu.wang <tywang@yitu-inc.intra>
2017-07-15 14:50:10 +0800

    deliver backup

--- deliver.sh
+++ deliver.sh
@@ -1,6 +1,11 @@
-home=/home/tywang/
+user=tywang
+home=/home/${user}/
+mv $home/.zshrc $home/.zshrc.bak
 cp .zshrc $home/.zshrc
+mv $home/.vimrc $home/.vimrc.bak
 cp .vimrc $home/.vimrc
+mv $home/.tmux.conf $home/.tmux.conf.bak
 cp .tmux.conf $home/.tmux.conf
-ficus=/home/tywang/ficus/
+ficus=/home/${user}/ficus/
+mv $ficus/CMakeLists.txt $ficus/CMakeLists.txt.bak
 cp CMakeLists.txt  $ficus/CMakeLists.txt
